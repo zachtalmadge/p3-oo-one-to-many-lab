aa3d7f97ac7165e06711c765999fd9dc8489f881
Zachary Talmadge <zach.talmadge.webdev@gmail.com>
2023-10-26 19:23:34 -0400

    pass tests

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,34 +1,57 @@
 class Pet:
     
     PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
-    
     all = []
     
     def __init__(self, name, pet_type, owner=None):
         self.name = name
+        self.pet_type = pet_type
         self.owner = owner
-        if pet_type not in Pet.PET_TYPES:
-            raise Exception
-        else:
-            self.pet_type = pet_type
-        Owner.all.append(self)
         Pet.all.append(self)
-
+        
+    @property
+    def pet_type(self):
+        return self._pet_type
+    
+    @pet_type.setter
+    def pet_type(self, pet_type):
+        if pet_type not in self.PET_TYPES:
+            raise Exception("Not a valid pet type")
+        self._pet_type = pet_type
+        
+    @property
+    def owner(self):
+        return self._owner
+    
+    @owner.setter
+    def owner(self, owner):
+        if not (isinstance(owner, Owner) or not owner):
+            raise Exception('WRong')
+        self._owner = owner
+        
 class Owner:
     
-    all = []
     
     def __init__(self, name):
         self.name = name
         
     def pets(self):
-        print( [pet for pet in Owner.all])
+        return [pet for pet in Pet.all if pet.owner == self]
     
     def add_pet(self, pet):
-        if isinstance(pet, Pet):
-            Owner.all.append(pet)
-        else:
-            raise Exception
+        if not isinstance(pet, Pet):
+            raise Exception('Input object is not of type Pet')
+        pet.owner = self
     
     def get_sorted_pets(self):
-        return sorted(all, key=lambda x: x['name'])
+        return sorted(self.pets(), key=lambda pet: pet.name)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
